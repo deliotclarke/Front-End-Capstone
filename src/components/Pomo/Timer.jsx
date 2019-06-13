d3ee2880ff15dc2b3cf0b351ee237b53dca67c92
Daniel Clarke <deliotclarke@gmail.com>
2019-06-13 14:02:02 -0500

    after much frustration i finally have  a countdown that can use state to countdown from 10 seconds and show itself on the DOM. there's problem with stopping and starting again. may have to change the stop button to just a reset button.

--- src/components/Pomo/Timer.jsx
+++ src/components/Pomo/Timer.jsx
@@ -1,18 +1,65 @@
 import React, { Component } from 'react'
+import { Button, Container } from 'reactstrap'
+
+
 
 export default class Timer extends Component {
-
   state = {
-    tasks: this.props.tasks,
-    user: this.props.user
+    minutes: '00',
+    seconds: 10,
+    counting: false
   }
 
-  render() {
 
+
+  inASecond = () => {
+
+    if (this.state.counting) {
+
+      let secondsLeft = this.state.seconds
+      // if (totalSeconds < 10) {
+      //   this.setState({
+      //     seconds: "0" + this.state.seconds
+      //   })
+      // }
+
+      // if (totalSeconds === 0) {
+      //   this.setState({
+      //     counting: false,
+      //     seconds: '10'
+      //   })
+      // }
+      secondsLeft--;
+
+      this.setState({
+        seconds: secondsLeft
+      })
+      console.log(this.state.seconds)
+    }
+  }
+
+  handleStart = (e) => {
+    let totalSeconds = parseInt(this.state.seconds)
+
+    this.setState({
+      counting: !this.state.counting
+    })
+
+    setInterval(this.inASecond, 1000)
+  }
+
+  handleReset = e => {
+    console.log(e.target.value)
+  }
+
+
+  render() {
     return (
-      <>
-        <h1>Timer for: {this.state.user.name}</h1>
-      </>
-    )
+      <Container style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "6rem" }}>{this.state.minutes}:{this.state.seconds}</h1>
+        <Button value="start" onClick={(e) => { this.handleStart(e) }}>Start/Stop</Button>
+        <Button value="reset" onClick={(e) => { this.handleReset(e) }}>Reset</Button>
+      </Container>
+    );
   }
 }
