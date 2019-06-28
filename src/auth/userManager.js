f6392fb3b81e4cf0696908d144a8864612dbc58b
Daniel Clarke <deliotclarke@gmail.com>
2019-06-28 10:31:51 -0500

    one more merge conflict, another blank line removed... *eyeroll x 1000*

--- src/auth/userManager.js
+++ src/auth/userManager.js
@@ -49,10 +49,6 @@ export const savePhoto = (photoObj, userId) => {
 
 
 export const patchUser = (userObj, userId) => {
-<<<<<<< HEAD
-=======
-
->>>>>>> master
   return base.update(`users/${userId}`, {
     data: userObj
   })
